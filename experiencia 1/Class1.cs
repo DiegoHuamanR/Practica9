009714ce87517fa42409cfb7236374f4868c230a
lenin.aragon <70281862+lenin9992@users.noreply.github.com>
2021-11-10 13:32:50 -0500

    Add files via upload

---
+++ experiencia 1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Class1
+/// </summary>
+public class Product
+{
+    private int id;
+    private string name;
+    private decimal price;
+
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; } 
+    }
+    public decimal Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+}
